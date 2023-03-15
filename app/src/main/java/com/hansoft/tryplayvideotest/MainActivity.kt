2612964758d633fca86e2ac9e2fe816e4e9fbbf1
Michael <zhuqi2012@hotmail.com>
2023-03-15 21:28:18 +1100

    First commit

---
+++ app/src/main/java/com/hansoft/tryplayvideotest/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.hansoft.tryplayvideotest
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val uri = Uri.parse("android.resource://$packageName/${R.raw.map}")
+        vvVideo.setVideoURI(uri)
+        btnPlay.setOnClickListener {
+            if (!vvVideo.isPlaying) {
+                vvVideo.start()
+            }
+        }
+        btnPause.setOnClickListener {
+            if (vvVideo.isPlaying) {
+                vvVideo.pause()
+            }
+        }
+        btnReplay.setOnClickListener {
+            if (vvVideo.isPlaying) {
+                vvVideo.resume()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        vvVideo.suspend()
+    }
+}
---
+++ app/src/test/java/com/hansoft/tryplayvideotest/ExampleUnitTest.kt
@@ -0,0 +1,35 @@
+package com.hansoft.tryplayvideotest
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun infix_test()
+    {
+        if ("Hello Kotlin" beginsWith "Hello")
+        {
+            println("hello kotlin start with hello")
+        }
+
+        val list = listOf("Apple", "Banana", "Orange", "Pear", "Grape")
+        if (list has "Banana") {
+            println("list has banana")
+        }
+
+        val map = mapOf("Apple" with 1, "Banana" with 2, "Orange" with 3, "Pear" with 4,
+            "Grape" with 5)
+        println(map)
+    }
+}
